9e5cee3e643f782d177d582b22aa7a5b06ed68b3
zs <zs@bjpowernode.com>
2023-01-02 22:35:18 +0800

    555

--- src/test3.java
+++ src/test3.java
@@ -3,4 +3,9 @@ public class test3 {
     private int b;
     private int c;
     private int d;
+
+    public static void main(String[] args) {
+
+        System.out.println("hello word");
+    }
 }
