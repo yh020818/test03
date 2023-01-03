02155c7ef7cd70f797d7b89de151f69974162c33
zs <zs@bjpowernode.com>
2023-01-03 10:28:04 +0800

    www

--- src/test3.java
+++ src/test3.java
@@ -8,7 +8,7 @@ public class test3 {
 
         System.out.println("yh");
         System.out.println("yj");
-        System.out.println("zz");
-        System.out.println("xx");
+        System.out.println("qq");
+        System.out.println("mm");
     }
 }
